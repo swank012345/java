179183ab466c115162318d907a102dad8f4bed11
sipl <sipl@sipl-OptiPlex-760>
2019-06-17 19:28:57 +0530

    commit

---
+++ src/com/sipl/app/FactorialProgram.java
@@ -0,0 +1,19 @@
+package com.sipl.app;
+
+public class FactorialProgram 
+{
+	public static void main(String[] args) 
+	{
+		int i,fact = 1;
+		int number=10;
+		for(i=1;i<=number;i++) {
+			
+		
+		fact=fact+i;
+		
+		}
+		System.out.println("Factorial of a number is"+fact);
+		
+	}
+
+}
