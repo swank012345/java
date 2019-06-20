a78d5680ff3d26e420f113cebf446ff8874e0db1
sipl <sipl@sipl-OptiPlex-760>
2019-06-20 19:58:36 +0530

    IfElse

---
+++ src/com/sipl/app/Ifelse.java
@@ -0,0 +1,15 @@
+package com.sipl.app;
+
+public class Ifelse
+{
+	public static void main(String[] args) 
+	{
+	    int number=13;  
+	    if(number%2==0){  
+	        System.out.println("even number");  
+	    }else{  
+	        System.out.println("odd number");  
+	    }  
+	}  
+	}  
+
